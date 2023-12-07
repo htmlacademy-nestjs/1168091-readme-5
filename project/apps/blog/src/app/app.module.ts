c020a4ac0f1b81a651daf46ee96fd40fd5fbb684
Constantine <kandruk@ya.ru>
2023-12-07 11:26:27 +0700

    Разбиваем приложения на отдельные модули, сервисы, контроллеры

--- project/apps/blog/src/app/app.module.ts
+++ project/apps/blog/src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
+import { LikesModule } from './likes/likes.module';
+import { TagsModule } from './tags/tags.module';
+import { SearchModule } from './search/search.module';
 
 @Module({
-  imports: [],
+  imports: [PostsModule, CommentsModule, LikesModule, TagsModule, SearchModule],
   controllers: [],
   providers: [],
 })
--- project/apps/file-loader/src/app/app.module.ts
+++ project/apps/file-loader/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { FileLoaderController } from './file-loader.controller';
+import { FileLoaderService } from './file-loader.service';
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [FileLoaderController],
+  providers: [FileLoaderService],
 })
 export class AppModule {}
