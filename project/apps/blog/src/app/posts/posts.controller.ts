4d51733327cb07ee58963b28531e9af484bca329
Constantine <kandruk@ya.ru>
2023-12-11 17:13:59 +0700

    Добавлено описание методов контроллера  blog/posts

--- project/apps/blog/src/app/posts/posts.controller.ts
+++ project/apps/blog/src/app/posts/posts.controller.ts
@@ -1,10 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post';
 import { RepostDto } from './dto/repost.dto';
 import { PostsService } from './posts.service';
 import { fillDto } from '@project/shared/helpers';
 import { postTypeToRdoClass } from '../rdo/post';
 import { AbstractPostEntity } from './entities/abstract-post.entity';
+import { ApiResponse } from '@nestjs/swagger';
+import { CommentRdo } from '../comments/rdo/comment.rdo';
+import { AbstractPostRdo } from '../rdo/post/abstract-post.rdo';
 
 @Controller('post')
 export class PostsController {
@@ -13,12 +16,22 @@ export class PostsController {
     private readonly postService: PostsService
   ) {
   }
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'The post has been created.',
+    type: AbstractPostRdo,
+  })
   @Post('create')
   async create(@Body() createPostDto: CreatePostDto) {
     const newPost: AbstractPostEntity = await this.postService.createPost(createPostDto);
     return fillDto(postTypeToRdoClass[newPost.postType], newPost.toPOJO());
   }
 
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The post has been updated.',
+    type: AbstractPostRdo,
+  })
   @Put('update/:id')
   async update(
     @Body() updatePostDto: CreatePostDto,
@@ -27,6 +40,10 @@ export class PostsController {
     return await this.postService.updatePost(updatePostDto, id);
   }
 
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The post has been deleted.',
+  })
   @Delete('delete/:id')
   async delete(
     @Param('id') id: string
@@ -34,6 +51,10 @@ export class PostsController {
     return await this.postService.deletePost(id);
   }
 
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The post has been created.',
+  })
   @Post('/repost/:id')
   async repost(
     @Body() repostDto: RepostDto,
@@ -42,6 +63,11 @@ export class PostsController {
     return await this.postService.repost(id);
   }
 
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The post has been created.',
+    type: AbstractPostRdo
+  })
   @Get('/:id')
   async getPostById(
     @Param('id') id: string
